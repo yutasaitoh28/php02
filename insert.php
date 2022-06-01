c2effe609ea2b39b5956d14f88da367b62990efe
yutasaitoh28 <33260997+yutasaitoh28@users.noreply.github.com>
2022-06-01 09:00:49 +0900

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * 1. index.phpのフォームの部分がおかしいので、ここを書き換えて、
+ * insert.phpにPOSTでデータが飛ぶようにしてください。
+ * 2. insert.phpで値を受け取ってください。
+ * 3. 受け取ったデータをバインド変数に与えてください。
+ * 4. index.phpフォームに書き込み、送信を行ってみて、実際にPhpMyAdminを確認してみてください！
+ */
+
+//1. POSTデータ取得
+
+
+//2. DB接続します
+try {
+  //ID:'root', Password: 'root'
+  $pdo = new PDO('mysql:dbname=  ;charset=utf8;host=localhost','******','******');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+//３．データ登録SQL作成
+
+// 1. SQL文を用意
+$stmt = $pdo->prepare("INSERT INTO gs_an_table(id, name, email, text, indate)VALUES(NULL, :name, :email, :text, sysdate())");
+
+//  2. バインド変数を用意
+// Integer 数値の場合 PDO::PARAM_INT
+// String文字列の場合 PDO::PARAM_STR
+
+$stmt->bindValue('******', *****, ****************);
+$stmt->bindValue('******', *****, ****************);
+$stmt->bindValue('******', *****, ****************);
+
+//  3. 実行
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status === false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit('ErrorMessage:'.$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+
+
+}
+?>
